62f28d19336254a3bbff758d3231ad7a416ee7e8
Olivia Charles <ocharles92@gmail.com>
2021-03-10 11:35:15 -0500

    Created tests for market class

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,4 +6,85 @@ require './lib/vendor'
 require 'pry'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Market, @market
+    assert_equal "South Pearl Street Farmers Market"
+  end
+
+  def test_it_starts_with_no_vendors
+    assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = [@vendor1, @vendor2, @vendor3]
+    assert_equal expected, market.vendors
+  end
+
+  def test_it_can_tell_vendor_names
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, market.vendor_names
+  end
+
+  def test_it_can_tell_items_vendors_sell
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@vendor1, @vendor3],  @market.vendors_that_sell(@item1)
+
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+
+  def test_it_has_potential_revenue
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal 29.75, @vendor1.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75,  @vendor3.potential_revenue
+  end
 end
