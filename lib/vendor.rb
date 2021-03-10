3fccb7d7f8cc94e897c492744754f2e72fce0907
Olivia Charles <ocharles92@gmail.com>
2021-03-10 11:23:41 -0500

    Created methods for vendor to add and check stock

---
+++ lib/vendor.rb
@@ -0,0 +1,21 @@
+class Vendor
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name      = name
+    @inventory = Hash.new(0)
+  end
+
+  def add_item(item)
+    @inventory << item
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item,  amount)
+    @inventory[item] += amount
+  end
+end
