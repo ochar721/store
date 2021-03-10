53133d5f493e4b71a20a1ebd2a595cae701f2277
Olivia Charles <ocharles92@gmail.com>
2021-03-10 11:14:44 -0500

    Creted all tests for vendor class

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,35 @@ require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+   vendor = Vendor.new("Rocky Mountain Fresh")
+   assert_instance_of Vendor, vendor
+   assert_equal "Rocky Mountain Fresh", vendor.name
+ end
+
+ def test_it_starts_with_no_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal ({}), vendor.inventory
+ end
+
+ def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal 0, vendor.check_stock(item1)
+ end
+
+ def test_it_can_stock_items
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    vendor.stock(item1, 30)
+    assert_equal ({item1 => 30}), vendor.inventory
+    assert_equal 30,  vendor.check_stock(item1)
+
+    vendor.stock(item1, 25)
+    assert_equal 55, vendor.stock(item1)
+
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor.stock(item2, 12)
+    expected = {item1 => 55, item2 => 12}
+    assert_equal expected, vendor.inventory
+ end
 end
